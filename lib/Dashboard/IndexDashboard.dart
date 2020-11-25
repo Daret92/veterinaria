7aee51d628d078261a070ea52a67ea44965f52a6
Eneas33 <61323906+Eneas33@users.noreply.github.com>
2020-11-25 15:01:16 -0600

    Interfaz Index

--- lib/Dashboard/IndexDashboard.dart
+++ lib/Dashboard/IndexDashboard.dart
@@ -11,52 +11,188 @@ class _IndexDashboard extends State<IndexDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Index"),
-        centerTitle: true,
-      ),
+      //not_listed_location
+      //perm_identity person  person_pin
       body: Column(
         children: [
-          RaisedButton(
-              color: Colors.blue,
-              child: Icon(Icons.warning, color: Colors.white),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/Pages/MascotaPerdidaPage');
-              }),
-          RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Mi Veterinaria",
-                style: TextStyle(color: Colors.white),
+          //Boton De Redireccionamiento a mascotas perdidas
+          Container(
+            padding: EdgeInsets.only(top: 14, right: 40, bottom: 40),
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              icon: Icon(
+                Icons.priority_high,
+                size: 80,
               ),
               onPressed: () {
+                //Navigator.of(context).pushNamed('/Pages/MascotaPerdidaPage');
+              },
+            ),
+          ),
+          Container(
+            height: 200,
+            width: 300,
+            color: Colors.black,
+            child: Text(
+              'Aqui va el banner de fotos o carrusel',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          //Boton de Mi veterinaria
+          Container(
+            margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+            height: 90,
+            width: double.infinity,
+            child: GestureDetector(
+              onTap: () {
                 Navigator.of(context).pushNamed('/Pages/MiVeterinariaPage');
-              }),
-          RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Calendario",
-                style: TextStyle(color: Colors.white),
+              },
+              child: Container(
+                child: Container(
+                  height: 90,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Mi veterinaria",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ),
               ),
-              onPressed: () {
+            ),
+          ),
+          //Boton de Vacunas
+          Container(
+            margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+            height: 90,
+            width: double.infinity,
+            child: GestureDetector(
+              onTap: () {
                 Navigator.of(context).pushNamed('/Pages/CalendarioPage');
-              }),
-          RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Comunidad",
-                style: TextStyle(color: Colors.white),
+              },
+              child: Container(
+                child: Container(
+                  height: 90,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Vacunas",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ),
               ),
-              onPressed: () {
+            ),
+          ),
+          //Boton de Comunidad
+          Container(
+            margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+            height: 90,
+            width: double.infinity,
+            child: GestureDetector(
+              onTap: () {
                 Navigator.of(context).pushNamed('/Pages/ComunidadPage');
-              }),
-          RaisedButton(
-              color: Colors.blue,
-              child: Icon(Icons.settings, color: Colors.white),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/Pages/DatosMascotaPage');
-              }),
+              },
+              child: Container(
+                child: Container(
+                  height: 90,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Mi veterinaria",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+      //Estructura del la barra (menu) inferior de la pantalla
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        elevation: 10,
+        child: Container(
+          height: 70,
+          child: Row(
+            children: [
+              //Boton con icono redirecciona a la Pantalla de perfil del usuario
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    IconButton(
+                      icon: (Icon(
+                        Icons.person_pin,
+                        color: Colors.white,
+                        size: 25,
+                      )),
+                      onPressed: () {},
+                    ),
+                    Text(
+                      'Mi Perfil',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              //Boton con icono redirecciona a la pantalla de agregar una mascota.
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    IconButton(
+                      icon: (Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 25,
+                      )),
+                      onPressed: () {},
+                    ),
+                    Text(
+                      'Agregar Mascota',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
